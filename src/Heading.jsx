41e03172bdea3a1a33d3729ef71ee099ab35fd61
hunx <junaidhunain6@gmail.com>
2024-02-08 16:40:33 +0500

    fixed carousel swipe issue

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -13,7 +13,7 @@ return (
 
     
 
-                    <div  className="filler heading"  style={{ textAlign: 'center', zIndex: 10, backgroundSize: inView ? '100%' : '500%',  animationTimingFunction: 'ease-in', opacity: inView ? 1 : 0, filter: inView ? 'none': 'blur(10px)' ,transform: inView ? 'scale(1)' : 'scale(50)', width: inView ? 'auto' : '100vw', overflow: 'clip', transition: '2s', backgroundImage: background || "url(https://images.pexels.com/photos/4393860/pexels-photo-4393860.jpeg?cs=srgb&dl=pexels-mathias-reding-4393860.jpg&fm=jpg&h=200&w=200&fit=crop&_gl=1*wrrvu5*_ga*MTY4ODkyODE0MS4xNzAyMDM2Mzk4*_ga_8JE65Q40S6*MTcwNzIyMzY0OC4xNC4xLjE3MDcyMjM3MzguMC4wLjA.)" }}>
+                    <div  className="filler heading"  style={{ textAlign: 'center', zIndex: 10, backgroundSize: inView ? '100%' : '500%',  animationTimingFunction: 'ease-in', opacity: inView ? 1 : 0, filter: inView ? 'none': 'blur(10px)' ,transform: inView ? 'scale(1)' : 'scale(50)', width: inView ? 'auto' : '2vw', overflow: 'clip', transition: '2s', backgroundImage: background || "url(https://images.pexels.com/photos/4393860/pexels-photo-4393860.jpeg?cs=srgb&dl=pexels-mathias-reding-4393860.jpg&fm=jpg&h=200&w=200&fit=crop&_gl=1*wrrvu5*_ga*MTY4ODkyODE0MS4xNzAyMDM2Mzk4*_ga_8JE65Q40S6*MTcwNzIyMzY0OC4xNC4xLjE3MDcyMjM3MzguMC4wLjA.)" }}>
                         <h1 ref={ref}>{text}</h1>
                     </div>
     )
