99d8ea54539d8c06d717eb05cf5224a7cb5b06d9
hunx <junaidhunain6@gmail.com>
2024-03-26 02:46:12 +0500

    added fb pixel

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactPixel from 'react-facebook-pixel';
+
+const options = {
+  autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
+  debug: false, // enable logs
+};
+ReactPixel.init('1348685699328819', advancedMatching, options);
+
+ReactPixel.pageView(); // For tracking page view
+ReactPixel.track(event, data); // For tracking default events. More info about standard events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#standard-events
+ReactPixel.trackSingle('PixelId', event, data); // For tracking default events.
+ReactPixel.trackCustom(event, data); // For tracking custom events. More info about custom events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#custom-events
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
